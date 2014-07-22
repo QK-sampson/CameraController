12a773fa0b4ef7538f38ca3bf331e7f1a944eb25
mikaylawho <mikel.i.hensley@gmail.com>
2014-07-22 19:49:35 -0400

    Added TimeLapse capability to the CameraControllerWebcam.py script

--- CameraControllerWebcam.py
+++ CameraControllerWebcam.py
@@ -15,13 +15,13 @@ pir_pin=18
 io.setup(pir_pin, io.IN)
 
 webcamconfig = '/home/pi/CameraController/fswebcam.conf'
-filepath = '/media/3BAA-1848/webcam/python/'
+filepath = '/home/pi/CameraController/lvl1/lvl1_'
 maxpictures = 10
 
 def main():    
     keepGoing = True
     while keepGoing:         
-        choice = input("What do you want to do? Select 1 for single photo, 2 for motion sensor trigger. ")
+        choice = input("What do you want to do? Select 1 for single photo, 2 for motion sensor trigger, 3 for TimeLapse. ")
         if choice == "1":
             takeOnePhoto()
             print("done!")
@@ -30,24 +30,13 @@ def main():
             checkMotionSensor()
             print("done!")
             keepGoing = False
+        elif choice == "3":
+            takeTimeLapse(20,1)
+            print("done!")
+            keepGoing = False
         else:
             print ("You have not chosen a valid option. Try again.")
 
-def checkCamera():
-    global cameraCheck
-    isCameraConnected = False
-    cameraCheck = subprocess.check_output(['sudo','gphoto2','--auto-detect'])
-    print (cameraCheck)
-    #encoding the find string as UTF seems to have resolve the TypeError when I run with Python 3
-    if cameraCheck.find(('Nikon DSC D5100').encode("utf-8")) >= 0: #not found results in -1        
-        print ('Nikon DSC D5100 found')
-        isCameraConnected = True
-    else:
-        print ('Nikon DSC D5100 NOT found')
-        isCameraConnected = False   
-
-    #return isCameraConnected
-    return True #disabling the cameracheck to test the rest of the script
 
 def takeOnePhoto():
     global webcamconfig, filepath
@@ -58,11 +47,26 @@ def takeOnePhoto():
         
     except:
         print ("Oops something went wrong taking a single photo! ")
-        print (sys.exc_info())
-        print (sys.exc_type)
+ 
 
+def takeTimeLapse(frames,intervalSeconds):
+    global webcamconfig, filepath
+    try:
+        #take one picture and download it to the folder containing the script
+        #Need to add code to specify the name of the photo to avoid file name conflicts
+        #This command takes a time lapse but keeps overwritting the same filename.
+        #subprocess.check_output(['sudo','fswebcam', '-l', str(frames), '--offset', str(intervalSeconds),'-c',webcamconfig,filepath + str(time.time()) + '.jpeg'])
+        count = 0
+        while count < frames:
+            takeOnePhoto()
+            time.sleep(intervalSeconds)
+            count += 1
+    except:
+        print ("Oops something went wrong taking a single photo! ")
+       
 
 def checkMotionSensor():
+    #TODO: build in a way to camcel motion sensor monitering by pressing a key
     global pir_pin,maxpictures
     maxphotos = input("What is the maximum number of picture you want to be triggered by the motion sensor? ")
     maxpictures = int(maxphotos)
